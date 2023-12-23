7b10940185a2e8f17272d8ab5360c9808082a1dd
afonsolelis <afonsolelis@gmail.com>
2023-12-23 19:22:56 -0300

    mudança de interação

--- app.py
+++ app.py
@@ -21,20 +21,20 @@ except Exception as e:
     st.error("Erro ao conectar ao banco de dados PostgreSQL. Verifique suas credenciais.")
     st.stop()
 
-# Função para exibir vendas por CNPJ
-def display_sales_by_cnpj(cnpj):
-    cursor.execute(f"SELECT * FROM dados_api WHERE cnpj = '{cnpj}'")
+# Função para exibir vendas por CNPJ e data
+def display_sales_by_cnpj_and_date(cnpj, start_date, end_date):
+    cursor.execute(f"SELECT * FROM dados_api WHERE cnpj = '{cnpj}' AND sale_date BETWEEN '{start_date}' AND '{end_date}'")
     data = cursor.fetchall()
     if data:
         df = pd.DataFrame(data, columns=['amount', 'category_id', 'cnpj', 'sale_id', 'sale_date', 'value'])
-        st.write(f"Vendas para o CNPJ: {cnpj}")
+        st.write(f"Vendas para o CNPJ: {cnpj} no período de {start_date} a {end_date}")
         st.dataframe(df)
     else:
-        st.warning("Nenhuma venda encontrada para o CNPJ selecionado.")
+        st.warning("Nenhuma venda encontrada para o CNPJ e datas selecionados.")
 
 # Função para exibir gráfico de série temporal
-def display_time_series_plot(cnpj):
-    cursor.execute(f"SELECT sale_date, value FROM dados_api WHERE cnpj = '{cnpj}'")
+def display_time_series_plot(cnpj, start_date, end_date):
+    cursor.execute(f"SELECT sale_date, value FROM dados_api WHERE cnpj = '{cnpj}' AND sale_date BETWEEN '{start_date}' AND '{end_date}'")
     data = cursor.fetchall()
     if data:
         df = pd.DataFrame(data, columns=['sale_date', 'value'])
@@ -44,20 +44,42 @@ def display_time_series_plot(cnpj):
         plt.plot(df['sale_date'], df['value'])
         plt.xlabel('Data da Venda')
         plt.ylabel('Valor')
-        plt.title(f"Série Temporal de Vendas para o CNPJ: {cnpj}")
+        plt.title(f"Série Temporal de Vendas para o CNPJ: {cnpj} no período de {start_date} a {end_date}")
         st.pyplot(plt)
     else:
-        st.warning("Nenhuma venda encontrada para o CNPJ selecionado.")
+        st.warning("Nenhuma venda encontrada para o CNPJ e datas selecionados.")
+
+# Função para exibir os 10 CNPJs que mais vendem em um gráfico de barra horizontal
+def display_top_10_selling_cnpjs():
+    cursor.execute("SELECT cnpj, SUM(value) AS total_sales FROM dados_api GROUP BY cnpj ORDER BY total_sales DESC LIMIT 10")
+    data = cursor.fetchall()
+    if data:
+        df = pd.DataFrame(data, columns=['cnpj', 'total_sales'])
+        plt.figure(figsize=(10, 6))
+        plt.barh(df['cnpj'], df['total_sales'])
+        plt.xlabel('Total de Vendas')
+        plt.ylabel('CNPJ')
+        plt.title('Top 10 CNPJs que Mais Vendem')
+        st.pyplot(plt)
+    else:
+        st.warning("Nenhum dado encontrado para exibir o gráfico.")
 
 # Aplicativo Streamlit
-st.title("Visualização de Vendas por CNPJ")
+st.title("Visualização de Vendas por CNPJ e Data")
 
-# Exibir vendas por CNPJ
+# Seleção de CNPJ
 selected_cnpj = st.text_input("Digite o CNPJ desejado:")
-if st.button("Exibir Vendas"):
-    display_sales_by_cnpj(selected_cnpj)
 
-# Exibir gráfico de série temporal
-selected_cnpj_time_series = st.text_input("Digite o CNPJ para o gráfico de série temporal:")
+# Seleção de datas
+start_date = st.date_input("Selecione a data de início:")
+end_date = st.date_input("Selecione a data de término:")
+
+if st.button("Exibir Vendas"):
+    display_sales_by_cnpj_and_date(selected_cnpj, start_date, end_date)
+
 if st.button("Exibir Gráfico de Série Temporal"):
-    display_time_series_plot(selected_cnpj_time_series)
+    display_time_series_plot(selected_cnpj, start_date, end_date)
+
+# Exibir gráfico dos 10 CNPJs que mais vendem
+if st.button("Exibir Top 10 CNPJs que Mais Vendem"):
+    display_top_10_selling_cnpjs()
